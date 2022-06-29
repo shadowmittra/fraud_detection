78c1da9c2f4020351902a810179922201a87d582
shadowmittra <shadow.mittra@gmail.com>
2022-06-29 21:03:11 +0530

    added 3 more ml model code

---
+++ ml_model.py
@@ -0,0 +1,17 @@
+
+
+print("adding all ML related code hereS")
+
+def modl1():
+    print("adding code for ml model1")
+
+
+
+def modl2():
+    print("adding code for ml model2")
+
+
+def modl3():
+    print("adding code for ml model3")
+
+
